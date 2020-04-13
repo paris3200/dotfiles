7bed7f7ba1ff6ea305a3deb2db91b70787f8ca76
Jason Paris <paris3200@gmail.com>
2020-04-13 06:19:41 -0400

    Add bat

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -1,9 +1,12 @@
 alias vim=nvim
 alias v=nvim
 alias ls="exa --time-style=long-iso"
-
 alias gs="git status"
 alias ga="git add"
-
+alias gc="git commit"
 alias t=timetrap
+alias cat=bat
 
+
+# Config Alias
+alias mbsync="mbsync -c ~/.config/mbsync/mbsyncrc"
