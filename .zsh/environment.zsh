1e0e37f1a9f3e61a9718cf23dfaf94d6a223c01d
Jason Paris <paris3200@gmail.com>
2021-03-27 18:20:23 -0400

    Add environment.zsh

---
+++ .zsh/environment.zsh
@@ -0,0 +1,16 @@
+typeset -U PATH path
+path=("$HOME/bin" "/usr/bin" "$path[@]")
+export PATH
+
+export EDITOR="nvim"
+export READER="zathura"
+export TERMINAL="kitty"
+export BROWSER="qutebrowser"
+
+export GNUPGHOME=$HOME/.config/gpg/
+
+#Bat Config
+export BAT_THEME="base16"
+export LC_CTYPE=en_US.UTF-8
+
+
