7a4a7874ad8f5f30cdcc121a9e179a1ecbc8669a
Jason Paris <paris3200@gmail.com>
2021-08-20 09:16:30 -0400

    Add tmux, gcalcli

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,10 +1,8 @@
 alias cat=bat
-alias code="kitty --session ~/.config/kitty/session_code.conf"
 alias ga="git add"
 alias gc="git commit"
 alias gd="git diff"
 alias gs="git status"
-alias ldg="ledger -f ~/100_personal/10_19_administration/11_finances/11.01_ledger/ledger.ldg" 
 alias ll="exa --long --time-style=long-iso"
 alias lla="exa --long --all --time-style=long-iso"
 alias ls="exa --time-style=long-iso"
@@ -12,7 +10,9 @@ alias mutt=neomutt
 alias v=nvim
 alias vim=nvim
 alias vimf="cd ~/100_personal/10_19_administration/11_finances/11.01_ledger/; vim ledger.ldg" 
-alias yay=paru
+alias calm="gcalcli calm"
+alias tmux="tmux -f ~/.config/tmux/config"
+
 
 
 # Config Alias
