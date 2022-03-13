c3b9ef10ce9a401b388bf8d0bfefeb5af079c950
Jason Paris <paris3200@gmail.com>
2022-03-13 17:25:38 -0400

    Rename ledger file

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -9,9 +9,11 @@ alias ls="exa --time-style=long-iso"
 alias mutt=neomutt
 alias v=nvim
 alias vim=nvim
-alias vimf="cd ~/100_personal/10_19_administration/11_finances/11.01_ledger/; vim ledger.ldg" 
+alias vimf="cd ~/100_personal/10_19_administration/11_finances/11.01_ledger/; vim 2021.ldg" 
 alias calm="gcalcli calm"
+alias calw="gcalcli calw"
 alias tmux="tmux -f ~/.config/tmux/config"
+alias advent="cd ~/100_personal/30_39_programming/31_python/31.08_advent_of_code/code/2021/"
 
 
 
