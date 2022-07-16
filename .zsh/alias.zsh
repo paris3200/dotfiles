3eb2eb0240e23f8d2f2413997907b7458ca41a9b
Jason Paris <paris3200@gmail.com>
2022-07-15 21:57:38 -0400

    Update aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,22 +1,22 @@
+# Overwrites commands
 alias cat=bat
-alias ga="git add"
-alias gc="git commit"
-alias gd="git diff"
-alias gs="git status"
-alias ll="exa --long --time-style=long-iso"
-alias lla="exa --long --all --time-style=long-iso"
 alias ls="exa --time-style=long-iso"
 alias mutt=neomutt
 alias v=nvim
 alias vim=nvim
-alias vimf="cd ~/100_personal/10_19_administration/11_finances/11.01_ledger/; vim 2021.ldg" 
-alias calm="gcalcli calm"
-alias calw="gcalcli calw"
-alias tmux="tmux -f ~/.config/tmux/config"
-alias advent="cd ~/100_personal/30_39_programming/31_python/31.08_advent_of_code/code/2021/"
 
+# Alias
 
+# Git
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias gs="git status"
 
-# Config Alias
-alias config=dotbare
-
+alias gcalm="gcalcli calm"
+alias gcalw="gcalcli calw"
+alias ll="exa --long --time-style=long-iso"
+alias lla="exa --long --all --time-style=long-iso"
+alias t="todo.sh"
+alias ts="timesheet"
+alias vimf="tmux attach -t ledger" 
