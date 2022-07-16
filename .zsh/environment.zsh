c2fa3466e1c2c028629249121edc3191e37f6033
Jason Paris <paris3200@gmail.com>
2022-07-15 21:59:01 -0400

    Remove dotbare

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -1,20 +1,24 @@
 typeset -U PATH path
-path=("$HOME/bin" "/usr/bin" "$path[@]")
+path=("$HOME/bin" "/usr/bin" "/home/jason/.local/share/gem/ruby/3.0.0/bin" "/home/jason/.local/bin" "$path[@]")
 export PATH
 
+# Default Programs
 export EDITOR="nvim"
 export READER="zathura"
 export TERMINAL="kitty"
 export BROWSER="qutebrowser"
 
+# Configs
 export GNUPGHOME=$HOME/.config/gpg/
-
-#Bat Config
 export BAT_THEME="base16"
 export LC_CTYPE=en_US.UTF-8
+export GPG_TTY=${TTY}                   # Allows GPG to run in tmux
 
-#Dotbare
-export DOTBARE_DIR="$HOME/.cfg"
-export DOTBARE="$HOME"
+# Shortcut Alias
+export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+export vimconfig=$HOME/.config/nvim/init.lua
+
+# Wayland Specific Options
+export MOZ_ENABLE_WAYLAND=1 firefox
 
 
